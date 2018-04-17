742754fb23c0600f162f4fea298ade7a108f6780
Václav Pelíšek <peldax@gmail.com>
2018-04-17 13:49:48 +0200

    Create Robots.php

---
+++ src/Robots.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of Nepttune (https://www.peldax.com)
+ *
+ * Copyright (c) 2018 Václav Pelíšek (info@peldax.com)
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license. For more information, see
+ * <https://www.peldax.com>.
+ */
+
+declare(strict_types = 1);
+
+namespace Nepttune\Component;
+
+final class Robots extends BaseComponent
+{
+    /** @var array */
+    private $robots;
+
+    public function __construct(array $robots)
+    {
+        $this->robots = $robots;
+    }
+
+    protected function beforeRender() : void
+    {
+        $this->template->robots = $this->robots;
+    }
+}
